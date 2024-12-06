5899e690aa3d2788943d4930d35984359579a8e3
marc575 <mtatchou7@gmail.com>
2024-12-06 09:59:08 +0100

    stage

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -40,7 +40,7 @@ export function ClientLayout({ children }: ClientLayoutProps) {
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
         <main className="flex-1 overflow-auto bg-gray-50">
-          <div className="container mx-auto">
+          <div className="mx-auto">
             {children}
           </div>
         </main>
